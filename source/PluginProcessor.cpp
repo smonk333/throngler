8359dbdc39ccf47ff71a39ce2663b2993e0c17c3
smoke <gboling829@gmail.com>
2025-05-23 12:48:47 -0400

    added logic for soft clipping tanh distortion + a tone filter

--- source/PluginProcessor.cpp
+++ source/PluginProcessor.cpp
@@ -144,33 +144,32 @@ bool PluginProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
 void PluginProcessor::processBlock (juce::AudioBuffer<float>& buffer,
                                               juce::MidiBuffer& midiMessages)
 {
-    juce::ignoreUnused (midiMessages);
+    const float gain = *gainParam;
+    const float drive = *driveParam;
+    const float tone = *toneParam;
 
-    juce::ScopedNoDenormals noDenormals;
-    auto totalNumInputChannels  = getTotalNumInputChannels();
-    auto totalNumOutputChannels = getTotalNumOutputChannels();
+    auto numChannels = buffer.getNumChannels();
+    auto numSamples = buffer.getNumSamples();
 
-    // In case we have more outputs than inputs, this code clears any output
-    // channels that didn't contain input data, (because these aren't
-    // guaranteed to be empty - they may contain garbage).
-    // This is here to avoid people getting screaming feedback
-    // when they first compile a plugin, but obviously you don't need to keep
-    // this code if your algorithm always overwrites all the output channels.
-    for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
-        buffer.clear (i, 0, buffer.getNumSamples());
+    // update the filter coefficients (low-pass filter after distortion)
+    *toneFilter.state = *juce::dsp::IIR::Coefficients<float>::makeLowPass(getSampleRate(), tone);
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
+    juce::dsp::AudioBlock<float> block(buffer);
+
+    for (int ch = 0; ch < buffer.getNumChannels(); ++ch)
     {
-        auto* channelData = buffer.getWritePointer (channel);
-        juce::ignoreUnused (channelData);
-        // ..do something to the data...
+        auto* channelData = buffer.getWritePointer(ch);
+        for (int sa = 0; sa < buffer.getNumSamples(); ++sa)
+        {
+            float in = channelData[sa];
+            float distort = std::tanh(drive * in); // controls how aggressive distortion is
+            channelData[sa] = gain * distort;         // gain controls output level
+        }
     }
+
+    // apply the tone filter
+    toneFilter.process(juce::dsp::ProcessContextReplacing(block));
+
 }
 
 //==============================================================================
